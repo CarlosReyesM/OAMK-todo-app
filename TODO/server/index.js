ef799b9c4b40eaf87b9dafac2e4b1f24842979c2
Carlos Reyes Muñoz <carlos.reyes.m@outlook.com>
2023-03-15 18:41:56 +0200

    create database connection and post new task API

--- TODO/server/index.js
+++ TODO/server/index.js
@@ -5,10 +5,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const pg_1 = require("pg");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 const port = 3001;
-app.get('/', (req, res) => {
-    res.status(200).json({ result: 'success' });
+app.get("/", (req, res) => {
+    const pool = openDb();
+    pool.query("select * from task", (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json(result.rows);
+    });
+});
+const openDb = () => {
+    const pool = new pg_1.Pool({
+        user: "postgres",
+        host: "host.docker.internal",
+        database: "todo",
+        password: "postgrespw",
+        port: 49153,
+    });
+    return pool;
+};
+app.post("/new", (req, res) => {
+    const pool = openDb();
+    pool.query("insert into task (description) values($1) returning *", [req.body.description], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ id: result.rows[0].id });
+    });
 });
 app.listen(port);
