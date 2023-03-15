1e40e6c15bc2d2783fafc28ecd5d76c01aa512a4
Carlos Reyes Muñoz <carlos.reyes.m@outlook.com>
2023-03-15 18:13:44 +0200

    create server

---
+++ TODO/server/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import cors from 'cors';
+import { Pool } from 'pg';
+
+const app: Express = express();
+app.use(cors());
+
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({result: 'success'});
+});
+
+app.listen(port);
